0d2dc325151d3b7e451a7b7a7699fcf9edb6baba
Paulo Pereira <palbp.giga@gmail.com>
2023-03-03 14:37:14 +0000

    Código escrito durante a segunda parte da aula

--- PacMan/src/main/kotlin/pacman/Main.kt
+++ PacMan/src/main/kotlin/pacman/Main.kt
@@ -1,5 +1,15 @@
 package pacman
 
+import pacman.domain.Coordinate
+import pacman.domain.Hero
+
 fun main() {
-    println("Hello, World!")
+    val origin = Coordinate(row = 0, column = 0)
+    val other = Coordinate(row = 1, column = 1)
+
+    val hero = Hero(at = origin)
+    println(hero)
+    println(hero.at)
+    println(hero.at.row)
+    println(hero.at.column)
 }
