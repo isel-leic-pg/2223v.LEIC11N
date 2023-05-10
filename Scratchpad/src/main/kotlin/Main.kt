13e791fcd3c16edd3a0913b189b2b60b532f285c
Paulo Pereira <palbp.giga@gmail.com>
2023-05-10 23:04:21 +0100

    Adds code written during the last lecture

--- Scratchpad/src/main/kotlin/Main.kt
+++ Scratchpad/src/main/kotlin/Main.kt
@@ -1,52 +1,29 @@
 
 /**
- * Script: Topic - Lists
- * 1. Let's create a list of integers using the listOf function
- * 1.1. Let´s print the list
- * 1.2. Let's print the even elements of the list
- * 1.3. Let's print the odd elements of the list
- * 1.4. Let's print the result of multiplying by 2 all the elements of the list
- * 2. Let's create a histogram of occurrences of letters in a phrase.
- * 2.1. We start by producing a list of integers (not the best approach)
- *      - For this, we look at List and MutableList
- *      - And we implement it using the same approach as before: a local mutable list to which we add the count
- * 2.2. Next, we produce a list of Occurrences, a data class containing the letter and the count
- * 2.3. Finally, we use buildList
- * 3. What's the problem with our implementation? How many times do we iterate on the input phrase? Can we do better?
+ * Script: Topic - Immutability and Mutability
+ * 1. Let's refine our mental model of execution
+ * 1.1. Local variables and local values, revisited
+ * 1.1.1 Simple values
+ * 1.1.2 Compound values
+ * 1.1.3 Variables of simple types and of compound types
+ * 1.3. What is the difference between a value and a variable?
+ * 2. Remember: the substitution model of execution only works for immutable values
+ * 3. What about compound types? What does it mean to be immutable?
+ * 3.1. What are the consequences of mutability in compound types?
+ * 3.2. Value types and reference types
+ * 3.2. Identity and equivalence
+ * 4. Conclusion: prefer immutable values and immutable compound types, for the sake of simplicity
  */
-
-data class Occurrence(val letter: Char, val count: Int)
-
-fun histogram(word: String): List<Occurrence> {
-    val alphabet = 'a' .. 'z'
-    val counts = MutableList(size = alphabet.count()) { 0 }
-
-    word.lowercase().forEach {
-        val letterIndex = alphabet.indexOf(it)
-        counts[letterIndex] += 1
-    }
-
-    return counts.mapIndexed { index: Int, elem: Int ->
-        Occurrence(letter = alphabet.elementAt(index), count = elem)
-    }
-}
-
 fun main() {
+    var l1 = mutableListOf(1, 2)
+    val l2 = l1
+    l1.add(3)
+    println(l1)
+    println(l2)
 
-    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val evenList = list.filter { it % 2 == 0 }
+    l1 = mutableListOf(4)
 
-    val squares = list.map { it * it }
-
-    val anotherList = list
-        .filter { it % 2 == 0 }
-        .map { (it * it).toString() }
-
-    println(list)
-    println(evenList)
-    println(squares)
-    println(anotherList)
-
-    val counts = histogram("GLORIOSO")
-    println(counts)
+    l1.add(5)
+    println(l1)
+    println(l2)
 }
